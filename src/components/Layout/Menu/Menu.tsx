264ad05c148e54287c387d47acc43e36ffa811d9
AgusjkDev <agusjkdev7@gmail.com>
2023-01-25 20:30:24 -0300

    Change Menu anchor to Link and wrapped in h1 tag

--- src/components/Layout/Menu/Menu.tsx
+++ src/components/Layout/Menu/Menu.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import MenuButton from "./MenuButton";
 import { menuButtons } from "data";
 
@@ -12,12 +14,9 @@ export default function Menu() {
 
     return (
         <div className="fixed bottom-0 flex w-full items-center justify-evenly bg-white py-3.5 md:static md:bottom-auto md:w-1/3 md:max-w-xs md:flex-col md:justify-start md:gap-4 md:py-8">
-            <a
-                href="/"
-                className="order-3 text-center text-xl font-bold text-secondary hover:text-secondary-dark md:order-none md:text-2xl"
-            >
-                Shade Shop
-            </a>
+            <h1 className="order-3 text-center text-xl font-bold text-secondary hover:text-secondary-dark md:order-none md:text-2xl">
+                <Link href="/">Shade Shop</Link>
+            </h1>
 
             {menuButtons.map(menuButton => {
                 const { key, href } = menuButton;
