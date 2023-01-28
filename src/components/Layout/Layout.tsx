6805f653a9bf8606bd5650dd8eb520c63360ecb0
AgusjkDev <agusjkdev7@gmail.com>
2023-01-28 00:22:04 -0300

    Change Layout component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,11 +6,9 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
     return (
-        <div className="flex flex-col bg-primary-lightest md:flex-row">
-            <div className="px-4 md:order-2 md:flex-1 md:px-0">
-                <div className="mx-auto grid min-h-screen w-full place-content-center md:w-[85%]">
-                    {children}
-                </div>
+        <div className="flex min-h-screen flex-col bg-primary-lightest md:flex-row">
+            <div className="md:scrollbar px-4 md:order-2 md:max-h-screen md:flex-1 md:px-0 md:[overflow-y:overlay]">
+                <div className="w-full pb-16 md:mx-auto md:w-[85%] md:pb-0">{children}</div>
             </div>
 
             <Menu />
