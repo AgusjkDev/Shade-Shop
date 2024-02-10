ca495ee1cd2d8e7e432f2f13c88be3f11b9862cc
AgusjkDev <agusjkdev7@gmail.com>
2024-02-10 01:38:16 -0300

    add hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,28 @@
+import Image from "next/image";
+
 export default function Index() {
-    return null;
+    return (
+        <>
+            <main className="relative grid flex-1 place-items-center">
+                <Image
+                    alt="Imagen de fondo sobre personas vistiendo prendas con estilo"
+                    src="/hero.jpg"
+                    className="pointer-events-none select-none object-cover"
+                    fill
+                    priority
+                    quality={100}
+                />
+
+                <div className="absolute flex flex-col gap-y-6 px-4">
+                    <h1 className="text-shadow text-center text-title uppercase text-white shadow-black/20">
+                        Shade Shop
+                    </h1>
+
+                    <h2 className="text-shadow text-center text-subtitle uppercase text-white shadow-black/20">
+                        Tu tienda de confianza
+                    </h2>
+                </div>
+            </main>
+        </>
+    );
 }
