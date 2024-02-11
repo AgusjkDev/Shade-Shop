0fc038b21f301f0fb9cab900c4c155af3b5b30b8
AgusjkDev <agusjkdev7@gmail.com>
2024-02-11 11:53:29 -0300

    add formatDate util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function formatDate(date: Date, variant: "small" | "normal" | "large" = "normal") {
+    const options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        weekday: variant === "large" ? "long" : undefined,
+        hour: variant === "normal" || variant === "large" ? "numeric" : undefined,
+        minute: variant === "normal" || variant === "large" ? "numeric" : undefined,
+    } satisfies Intl.DateTimeFormatOptions;
+
+    return new Intl.DateTimeFormat("es-ES", options).format(date);
+}
